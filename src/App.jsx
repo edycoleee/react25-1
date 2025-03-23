e40ca718e42a931388afc5bd32a89a3ce82364f6
silmi <silmi.ayra@gmail.com>
2025-03-23 12:20:08 +0700

    awal

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,27 @@
 //src/App.jsx
-import React, { useState } from 'react';
-import SiswaForm from './components/SiswaForm';
-import SiswaList from './components/SiswaList';
-import { nanoid } from 'nanoid';
-
-let tbSiswa = [
-  {
-    id: 1,
-    siswaName: 'Budi',
-    siswaAlamat: 'Jakarta',
-  },
-  {
-    id: 2,
-    siswaName: 'Andi',
-    siswaAlamat: 'Bandung',
-  },
-  {
-    id: 3,
-    siswaName: 'Cindy',
-    siswaAlamat: 'Surabaya',
-  },
-];
 
 const App = () => {
-  // const [tbSiswa, setTbSiswa] = useState([]);
-  // const [editSiswa, setEditSiswa] = useState(null);
-
-  // const addSiswa = (siswa) => {
-  //   siswa.id = nanoid();
-  //   setTbSiswa([...tbSiswa, siswa]);
-  // };
-
-  // const updateSiswa = (updatedSiswa) => {
-  //   setTbSiswa(tbSiswa.map((siswa) => (siswa.id === updatedSiswa.id ? updatedSiswa : siswa)));
-  // };
-
-  // const deleteSiswa = (id) => {
-  //   setTbSiswa(tbSiswa.filter((siswa) => siswa.id !== id));
-  // };
-
-  const deleteSiswa = (id) => {
-    console.log("DELETE ID : ", id);
-  };
-  const setEditSiswa = (siswa) => {
-    console.log("EDIT SISWA :", siswa);
-  }
 
   return (
     <div>
-      <h1>Aplikasi Siswa</h1>
-      {/* <SiswaForm addSiswa={addSiswa} editSiswa={editSiswa} updateSiswa={updateSiswa} setEditSiswa={setEditSiswa} /> */}
-      <SiswaList tbSiswa={tbSiswa} setEditSiswa={setEditSiswa} deleteSiswa={deleteSiswa} />
+      <fieldset>
+        <legend>
+          JUDUL - App
+        </legend>
+        <h1>Aplikasi Siswa</h1>
+      </fieldset> <br />
+      <fieldset>
+        <legend>
+          FORM - AddSiswa
+        </legend>
+        <h3>Add Siswa</h3>
+      </fieldset><br />
+      <fieldset>
+        <legend>
+          DATA - EditSiswa,Delete
+        </legend>
+        <h3>DAFTAR SISWA</h3>
+      </fieldset>
     </div>
   );
 };
